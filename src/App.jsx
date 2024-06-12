a52d7ed1fb3b00517d6278c53a9b1981510eec1e
frostAdarsh <adarshmani017@gmail.com>
2024-06-12 22:12:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Todo from "./Components/Todo";
+
+function App() {
+  return <>
+
+  <div className=" bg-stone-700 grid py-4 min-h-screen">
+
+  <Todo/>
+  </div>
+  
+  </>;
+}
+
+export default App;
